faf5ad25225e091cd944b9f40a00af51efffce62
Lirapia Zaima <lirapiazaima@gmail.com>
2025-10-03 22:04:51 +0000

    Add all the chapters for Genesis and all their verses

--- src/lib/bible/kjv-old-testament.ts
+++ src/lib/bible/kjv-old-testament.ts
@@ -133,6 +133,130 @@ export const oldTestament: Book[] = [
                         "text": "And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day."
                     }
                 ]
+            },
+            {
+                "chapter": 2,
+                "verses": [
+                    { "verse": 1, "text": "Thus the heavens and the earth were finished, and all the host of them." },
+                    { "verse": 2, "text": "And on the seventh day God ended his work which he had made; and he rested on the seventh day from all his work which he had made." },
+                    { "verse": 3, "text": "And God blessed the seventh day, and sanctified it: because that in it he had rested from all his work which God created and made." },
+                    { "verse": 4, "text": "These are the generations of the heavens and of the earth when they were created, in the day that the LORD God made the earth and the heavens," },
+                    { "verse": 5, "text": "And every plant of the field before it was in the earth, and every herb of the field before it grew: for the LORD God had not caused it to rain upon the earth, and there was not a man to till the ground." },
+                    { "verse": 6, "text": "But there went up a mist from the earth, and watered the whole face of the ground." },
+                    { "verse": 7, "text": "And the LORD God formed man of the dust of the ground, and breathed into his nostrils the breath of life; and man became a living soul." },
+                    { "verse": 8, "text": "And the LORD God planted a garden eastward in Eden; and there he put the man whom he had formed." },
+                    { "verse": 9, "text": "And out of the ground made the LORD God to grow every tree that is pleasant to the sight, and good for food; the tree of life also in the midst of the garden, and the tree of knowledge of good and evil." },
+                    { "verse": 10, "text": "And a river went out of Eden to water the garden; and from thence it was parted, and became into four heads." },
+                    { "verse": 11, "text": "The name of the first is Pison: that is it which compasseth the whole land of Havilah, where there is gold;" },
+                    { "verse": 12, "text": "And the gold of that land is good: there is bdellium and the onyx stone." },
+                    { "verse": 13, "text": "And the name of the second river is Gihon: the same is it that compasseth the whole land of Ethiopia." },
+                    { "verse": 14, "text": "And the name of the third river is Hiddekel: that is it which goeth toward the east of Assyria. And the fourth river is Euphrates." },
+                    { "verse": 15, "text": "And the LORD God took the man, and put him into the garden of Eden to dress it and to keep it." },
+                    { "verse": 16, "text": "And the LORD God commanded the man, saying, Of every tree of the garden thou mayest freely eat:" },
+                    { "verse": 17, "text": "But of the tree of the knowledge of good and evil, thou shalt not eat of it: for in the day that thou eatest thereof thou shalt surely die." },
+                    { "verse": 18, "text": "And the LORD God said, It is not good that the man should be alone; I will make him an help meet for him." },
+                    { "verse": 19, "text": "And out of the ground the LORD God formed every beast of the field, and every fowl of the air; and brought them unto Adam to see what he would call them: and whatsoever Adam called every living creature, that was the name thereof." },
+                    { "verse": 20, "text": "And Adam gave names to all cattle, and to the fowl of the air, and to every beast of the field; but for Adam there was not found an help meet for him." },
+                    { "verse": 21, "text": "And the LORD God caused a deep sleep to fall upon Adam, and he slept: and he took one of his ribs, and closed up the flesh instead thereof;" },
+                    { "verse": 22, "text": "And the rib, which the LORD God had taken from man, made he a woman, and brought her unto the man." },
+                    { "verse": 23, "text": "And Adam said, This is now bone of my bones, and flesh of my flesh: she shall be called Woman, because she was taken out of Man." },
+                    { "verse": 24, "text": "Therefore shall a man leave his father and his mother, and shall cleave unto his wife: and they shall be one flesh." },
+                    { "verse": 25, "text": "And they were both naked, the man and his wife, and were not ashamed." }
+                ]
+            },
+            {
+                "chapter": 3, "verses": [
+                    { "verse": 1, "text": "Now the serpent was more subtil than any beast of the field which the LORD God had made. And he said unto the woman, Yea, hath God said, Ye shall not eat of every tree of the garden?" },
+                    { "verse": 2, "text": "And the woman said unto the serpent, We may eat of the fruit of the trees of the garden:" },
+                    { "verse": 3, "text": "But of the fruit of the tree which is in the midst of the garden, God hath said, Ye shall not eat of it, neither shall ye touch it, lest ye die." },
+                    { "verse": 4, "text": "And the serpent said unto the woman, Ye shall not surely die:" },
+                    { "verse": 5, "text": "For God doth know that in the day ye eat thereof, then your eyes shall be opened, and ye shall be as gods, knowing good and evil." },
+                    { "verse": 6, "text": "And when the woman saw that the tree was good for food, and that it was pleasant to the eyes, and a tree to be desired to make one wise, she took of the fruit thereof, and did eat, and gave also unto her husband with her; and he did eat." },
+                    { "verse": 7, "text": "And the eyes of them both were opened, and they knew that they were naked; and they sewed fig leaves together, and made themselves aprons." },
+                    { "verse": 8, "text": "And they heard the voice of the LORD God walking in the garden in the cool of the day: and Adam and his wife hid themselves from the presence of the LORD God amongst the trees of the garden." },
+                    { "verse": 9, "text": "And the LORD God called unto Adam, and said unto him, Where art thou?" },
+                    { "verse": 10, "text": "And he said, I heard thy voice in the garden, and I was afraid, because I was naked; and I hid myself." },
+                    { "verse": 11, "text": "And he said, Who told thee that thou wast naked? Hast thou eaten of the tree, whereof I commanded thee that thou shouldest not eat?" },
+                    { "verse": 12, "text": "And the man said, The woman whom thou gavest to be with me, she gave me of the tree, and I did eat." },
+                    { "verse": 13, "text": "And the LORD God said unto the woman, What is this that thou hast done? And the woman said, The serpent beguiled me, and I did eat." },
+                    { "verse": 14, "text": "And the LORD God said unto the serpent, Because thou hast done this, thou art cursed above all cattle, and above every beast of the field; upon thy belly shalt thou go, and dust shalt thou eat all the days of thy life:" },
+                    { "verse": 15, "text": "And I will put enmity between thee and the woman, and between thy seed and her seed; it shall bruise thy head, and thou shalt bruise his heel." },
+                    { "verse": 16, "text": "Unto the woman he said, I will greatly multiply thy sorrow and thy conception; in sorrow thou shalt bring forth children; and thy desire shall be to thy husband, and he shall rule over thee." },
+                    { "verse": 17, "text": "And unto Adam he said, Because thou hast hearkened unto the voice of thy wife, and hast eaten of the tree, of which I commanded thee, saying, Thou shalt not eat of it: cursed is the ground for thy sake; in sorrow shalt thou eat of it all the days of thy life;" },
+                    { "verse": 18, "text": "Thorns also and thistles shall it bring forth to thee; and thou shalt eat the herb of the field;" },
+                    { "verse": 19, "text": "In the sweat of thy face shalt thou eat bread, till thou return unto the ground; for out of it wast thou taken: for dust thou art, and unto dust shalt thou return." },
+                    { "verse": 20, "text": "And Adam called his wife's name Eve; because she was the mother of all living." },
+                    { "verse": 21, "text": "Unto Adam also and to his wife did the LORD God make coats of skins, and clothed them." },
+                    { "verse": 22, "text": "And the LORD God said, Behold, the man is become as one of us, to know good and evil: and now, lest he put forth his hand, and take also of the tree of life, and eat, and live for ever:" },
+                    { "verse": 23, "text": "Therefore the LORD God sent him forth from the garden of Eden, to till the ground from whence he was taken." },
+                    { "verse": 24, "text": "So he drove out the man; and he placed at the east of the garden of Eden Cherubims, and a flaming sword which turned every way, to keep the way of the tree of life." }
+                ]
+            },
+            {
+                "chapter": 4, "verses": [
+                    { "verse": 1, "text": "And Adam knew Eve his wife; and she conceived, and bare Cain, and said, I have gotten a man from the LORD." },
+                    { "verse": 2, "text": "And she again bare his brother Abel. And Abel was a keeper of sheep, but Cain was a tiller of the ground." },
+                    { "verse": 3, "text": "And in process of time it came to pass, that Cain brought of the fruit of the ground an offering unto the LORD." },
+                    { "verse": 4, "text": "And Abel, he also brought of the firstlings of his flock and of the fat thereof. And the LORD had respect unto Abel and to his offering:" },
+                    { "verse": 5, "text": "But unto Cain and to his offering he had not respect. And Cain was very wroth, and his countenance fell." },
+                    { "verse": 6, "text": "And the LORD said unto Cain, Why art thou wroth? and why is thy countenance fallen?" },
+                    { "verse": 7, "text": "If thou doest well, shalt thou not be accepted? and if thou doest not well, sin lieth at the door. And unto thee shall be his desire, and thou shalt rule over him." },
+                    { "verse": 8, "text": "And Cain talked with Abel his brother: and it came to pass, when they were in the field, that Cain rose up against Abel his brother, and slew him." },
+                    { "verse": 9, "text": "And the LORD said unto Cain, Where is Abel thy brother? And he said, I know not: Am I my brother's keeper?" },
+                    { "verse": 10, "text": "And he said, What hast thou done? the voice of thy brother's blood crieth unto me from the ground." },
+                    { "verse": 11, "text": "And now art thou cursed from the earth, which hath opened her mouth to receive thy brother's blood from thy hand;" },
+                    { "verse": 12, "text": "When thou tillest the ground, it shall not henceforth yield unto thee her strength; a fugitive and a vagabond shalt thou be in the earth." },
+                    { "verse": 13, "text": "And Cain said unto the LORD, My punishment is greater than I can bear." },
+                    { "verse": 14, "text": "Behold, thou hast driven me out this day from the face of the earth; and from thy face shall I be hid; and I shall be a fugitive and a vagabond in the earth; and it shall come to pass, that every one that findeth me shall slay me." },
+                    { "verse": 15, "text": "And the LORD said unto him, Therefore whosoever slayeth Cain, vengeance shall be taken on him sevenfold. And the LORD set a mark upon Cain, lest any finding him should kill him." },
+                    { "verse": 16, "text": "And Cain went out from the presence of the LORD, and dwelt in the land of Nod, on the east of Eden." },
+                    { "verse": 17, "text": "And Cain knew his wife; and she conceived, and bare Enoch: and he builded a city, and called the name of the city, after the name of his son, Enoch." },
+                    { "verse": 18, "text": "And unto Enoch was born Irad: and Irad begat Mehujael: and Mehujael begat Methusael: and Methusael begat Lamech." },
+                    { "verse": 19, "text": "And Lamech took unto him two wives: the name of the one was Adah, and the name of the other Zillah." },
+                    { "verse": 20, "text": "And Adah bare Jabal: he was the father of such as dwell in tents, and of such as have cattle." },
+                    { "verse": 21, "text": "And his brother's name was Jubal: he was the father of all such as handle the harp and organ." },
+                    { "verse": 22, "text": "And Zillah, she also bare Tubalcain, an instructer of every artificer in brass and iron: and the sister of Tubalcain was Naamah." },
+                    { "verse": 23, "text": "And Lamech said unto his wives, Adah and Zillah, Hear my voice; ye wives of Lamech, hearken unto my speech: for I have slain a man to my wounding, and a young man to my hurt." },
+                    { "verse": 24, "text": "If Cain shall be avenged sevenfold, truly Lamech seventy and sevenfold." },
+                    { "verse": 25, "text": "And Adam knew his wife again; and she bare a son, and called his name Seth: For God, said she, hath appointed me another seed instead of Abel, whom Cain slew." },
+                    { "verse": 26, "text": "And to Seth, to him also there was born a son; and he called his name Enos: then began men to call upon the name of the LORD." }
+                ]
+            },
+            {
+                "chapter": 5, "verses": [
+                    { "verse": 1, "text": "This is the book of the generations of Adam. In the day that God created man, in the likeness of God made he him;" },
+                    { "verse": 2, "text": "Male and female created he them; and blessed them, and called their name Adam, in the day when they were created." },
+                    { "verse": 3, "text": "And Adam lived an hundred and thirty years, and begat a son in his own likeness, and after his image; and called his name Seth:" },
+                    { "verse": 4, "text": "And the days of Adam after he had begotten Seth were eight hundred years: and he begat sons and daughters:" },
+                    { "verse": 5, "text": "And all the days that Adam lived were nine hundred and thirty years: and he died." },
+                    { "verse": 6, "text": "And Seth lived an hundred and five years, and begat Enos:" },
+                    { "verse": 7, "text": "And Seth lived after he begat Enos eight hundred and seven years, and begat sons and daughters:" },
+                    { "verse": 8, "text": "And all the days of Seth were nine hundred and twelve years: and he died." },
+                    { "verse": 9, "text": "And Enos lived ninety years, and begat Cainan:" },
+                    { "verse": 10, "text": "And Enos lived after he begat Cainan eight hundred and fifteen years, and begat sons and daughters:" },
+                    { "verse": 11, "text": "And all the days of Enos were nine hundred and five years: and he died." },
+                    { "verse": 12, "text": "And Cainan lived seventy years, and begat Mahalaleel:" },
+                    { "verse": 13, "text": "And Cainan lived after he begat Mahalaleel eight hundred and forty years, and begat sons and daughters:" },
+                    { "verse": 14, "text": "And all the days of Cainan were nine hundred and ten years: and he died." },
+                    { "verse": 15, "text": "And Mahalaleel lived sixty and five years, and begat Jared:" },
+                    { "verse": 16, "text": "And Mahalaleel lived after he begat Jared eight hundred and thirty years, and begat sons and daughters:" },
+                    { "verse": 17, "text": "And all the days of Mahalaleel were eight hundred ninety and five years: and he died." },
+                    { "verse": 18, "text": "And Jared lived an hundred sixty and two years, and he begat Enoch:" },
+                    { "verse": 19, "text": "And Jared lived after he begat Enoch eight hundred years, and begat sons and daughters:" },
+                    { "verse": 20, "text": "And all the days of Jared were nine hundred sixty and two years: and he died." },
+                    { "verse": 21, "text": "And Enoch lived sixty and five years, and begat Methuselah:" },
+                    { "verse": 22, "text": "And Enoch walked with God after he begat Methuselah three hundred years, and begat sons and daughters:" },
+                    { "verse": 23, "text": "And all the days of Enoch were three hundred sixty and five years:" },
+                    { "verse": 24, "text": "And Enoch walked with God: and he was not; for God took him." },
+                    { "verse": 25, "text": "And Methuselah lived an hundred eighty and seven years, and begat Lamech." },
+                    { "verse": 26, "text": "And Methuselah lived after he begat Lamech seven hundred eighty and two years, and begat sons and daughters:" },
+                    { "verse": 27, "text": "And all the days of Methuselah were nine hundred sixty and nine years: and he died." },
+                    { "verse": 28, "text": "And Lamech lived an hundred eighty and two years, and begat a son:" },
+                    { "verse": 29, "text": "And he called his name Noah, saying, This same shall comfort us concerning our work and toil of our hands, because of the ground which the LORD hath cursed." },
+                    { "verse": 30, "text": "And Lamech lived after he begat Noah five hundred ninety and five years, and begat sons and daughters:" },
+                    { "verse": 31, "text": "And all the days of Lamech were seven hundred seventy and seven years: and he died." },
+                    { "verse": 32, "text": "And Noah was five hundred years old: and Noah begat Shem, Ham, and Japheth." }
+                ]
             }
         ]
     },
@@ -223,8 +347,7 @@ export const oldTestament: Book[] = [
                         "text": "Therefore God dealt well with the midwives: and the people multiplied, and waxed very mighty."
                     },
                     {
-                        "verse": 21,
-                        "text": "And it came to pass, because the midwives feared God, that he made them houses."
+                        "verse": 21, "text": "And it came to pass, because the midwives feared God, that he made them houses."
                     },
                     {
                         "verse": 22,
