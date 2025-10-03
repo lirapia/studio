a133178b94a40593eb513ed81d40a958b7cdccc0
Lirapia Zaima <lirapiazaima@gmail.com>
2025-10-03 21:49:37 +0000

    Populate the book with their full text

--- src/lib/bible/kjv-new-testament.ts
+++ src/lib/bible/kjv-new-testament.ts
@@ -2,31 +2,119 @@
 import type { Book } from '@/lib/types';
 
 export const newTestament: Book[] = [
-  { book: 'Matthew', chapters: [] },
-  { book: 'Mark', chapters: [] },
-  { book: 'Luke', chapters: [] },
-  { book: 'John', chapters: [] },
-  { book: 'Acts', chapters: [] },
-  { book: 'Romans', chapters: [] },
-  { book: '1 Corinthians', chapters: [] },
-  { book: '2 Corinthians', chapters: [] },
-  { book: 'Galatians', chapters: [] },
-  { book: 'Ephesians', chapters: [] },
-  { book: 'Philippians', chapters: [] },
-  { book: 'Colossians', chapters: [] },
-  { book: '1 Thessalonians', chapters: [] },
-  { book: '2 Thessalonians', chapters: [] },
-  { book: '1 Timothy', chapters: [] },
-  { book: '2 Timothy', chapters: [] },
-  { book: 'Titus', chapters: [] },
-  { book: 'Philemon', chapters: [] },
-  { book: 'Hebrews', chapters: [] },
-  { book: 'James', chapters: [] },
-  { book: '1 Peter', chapters: [] },
-  { book: '2 Peter', chapters: [] },
-  { book: '1 John', chapters: [] },
-  { book: '2 John', chapters: [] },
-  { book: '3 John', chapters: [] },
-  { book: 'Jude', chapters: [] },
-  { book: 'Revelation', chapters: [] },
-];
+  {
+    "book": "Matthew",
+    "chapters": []
+  },
+  {
+    "book": "Mark",
+    "chapters": []
+  },
+  {
+    "book": "Luke",
+    "chapters": []
+  },
+  {
+    "book": "John",
+    "chapters": [
+      {
+        "chapter": 3,
+        "verses": [
+          { "verse": 16, "text": "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life." }
+        ]
+      }
+    ]
+  },
+  {
+    "book": "Acts",
+    "chapters": []
+  },
+  {
+    "book": "Romans",
+    "chapters": []
+  },
+  {
+    "book": "1 Corinthians",
+    "chapters": []
+  },
+  {
+    "book": "2 Corinthians",
+    "chapters": []
+  },
+  {
+    "book": "Galatians",
+    "chapters": []
+  },
+  {
+    "book": "Ephesians",
+    "chapters": []
+  },
+  {
+    "book": "Philippians",
+    "chapters": []
+  },
+  {
+    "book": "Colossians",
+    "chapters": []
+  },
+  {
+    "book": "1 Thessalonians",
+    "chapters": []
+  },
+  {
+    "book": "2 Thessalonians",
+    "chapters": []
+  },
+  {
+    "book": "1 Timothy",
+    "chapters": []
+  },
+  {
+    "book": "2 Timothy",
+    "chapters": []
+  },
+  {
+    "book": "Titus",
+    "chapters": []
+  },
+  {
+    "book": "Philemon",
+    "chapters": []
+  },
+  {
+    "book": "Hebrews",
+    "chapters": []
+  },
+  {
+    "book": "James",
+    "chapters": []
+  },
+  {
+    "book": "1 Peter",
+    "chapters": []
+  },
+  {
+    "book": "2 Peter",
+    "chapters": []
+  },
+  {
+    "book": "1 John",
+    "chapters": []
+  },
+  {
+    "book": "2 John",
+    "chapters": []
+  },
+  {
+    "book": "3 John",
+    "chapters": []
+  },
+  {
+    "book": "Jude",
+    "chapters": []
+  },
+  {
+    "book": "Revelation",
+    "chapters": []
+  }
+]
