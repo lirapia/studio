ffae765a7922612daed4115af9d235d64c66aa4d
Lirapia Zaima <lirapiazaima@gmail.com>
2025-10-06 22:56:11 +0000

    Give the light and dark mode a very visually appealing transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,18 +15,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="font-body antialiased">
+    <html lang="en" suppressHydrationWarning className="antialiased">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:opsz,wght@6..72,400;6..72,700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <body className={cn('font-body')}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
-          disableTransitionOnChange
         >
           {children}
           <Toaster />
