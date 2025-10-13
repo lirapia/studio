5fbcc738477be0b0d081e3d7f02fbbdd1f60f113
Lirapia Zaima <lirapiazaima@gmail.com>
2025-10-13 20:44:55 +0000

    Add this "<script>(function(s){s.dataset.zone='9956044',s.src='https://f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,12 +23,15 @@ export default function RootLayout({
         <link href="https://www.googletagmanager.com/gtag/js?id=G-Q3W38382J2" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:opsz,wght@6..72,400;6..72,700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
         <Script
-          id="monetag-script"
+          id="monetag-script-1"
           src="//pl24231845.highcpmgate.com/21f4229548d1921316b2505658518b03/invoke.js"
           strategy="beforeInteractive"
           data-cfasync="false"
           async
         />
+        <Script id="monetag-script-2" strategy="beforeInteractive">
+          {`(function(s){s.dataset.zone='9956044',s.src='https://forfrogadiertor.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
+        </Script>
       </head>
       <body className={cn('font-body')}>
         <ThemeProvider
